dfcb096f3f5617d131272a7987788865a526884d
Manuel Castañón-Puga <puga@uabc.edu.mx>
2024-02-15 13:40:19 -0800

    Mi codigo generado con VP

---
+++ Self_Service_Machine/Scaner.java
@@ -0,0 +1,8 @@
+package Self_Service_Machine;
+
+public class Scaner {
+	private Object _scanerId;
+	public void scan() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ Self_Service_Machine/SelfServiceMachine.java
@@ -0,0 +1,9 @@
+package Self_Service_Machine;
+
+public class SelfServiceMachine {
+	private Object _machineID;
+
+	public void scanProduct() {
+		throw new UnsupportedOperationException();
+	}
+}
